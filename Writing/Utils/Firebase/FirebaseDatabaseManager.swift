d0f6d8b0a04597a6555369fb1d07e05a3ec56f58
Yevgeniy Ivanov <anigeka@gmail.com>
2022-08-26 10:46:21 -0400

    add firebasedatabase manager

--- Writing/Utils/Firebase/FirebaseDatabaseManager.swift
+++ Writing/Utils/Firebase/FirebaseDatabaseManager.swift
@@ -5,4 +5,28 @@
 //  Created by Yevgeniy Ivanov on 8/25/22.
 //
 
-import Foundation
+import FirebaseDatabase
+
+class FirebaseDatabaseManager {
+    
+    static let shared = FirebaseDatabaseManager()
+    
+    private init() {}
+    
+    private let reference = Database.database().reference()
+    
+    func getInstance(of value: String, completion: @escaping () -> ()) {
+        reference.child(value).observeSingleEvent(of: .value) { snapshot in
+            print(snapshot)
+        }
+    }
+    
+    func setInstance(of value: String, completion: @escaping () -> ()) {
+        let object: [String: Any] = [
+            "name": "iOS Academy" as NSObject,
+            "Youtube": "yes"
+        ]
+        reference.child(value).setValue(object)
+    }
+    
+}
--- Writing/ViewController.swift
+++ Writing/ViewController.swift
@@ -10,35 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let database = Database.database().reference()
+    private let databaseManager = FirebaseDatabaseManager.shared
     
-    private let firebaseManager = FirebaseAuthManager.shared
+    private let authManager = FirebaseAuthManager.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if firebaseManager.isUserSignedIn {
-            // Navigate to users list
+        if authManager.isUserSignedIn {
+            databaseManager.getInstance(of: "Something") {
+                print("hi")
+            }
         } else {
             let vc = AuthenticationViewController()
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: true)
         }
-//        let button = UIButton(frame: CGRect(x: 20, y: 200, width: view.frame.size.width - 40, height: 50))
-//        button.setTitle("Add Entry", for: .normal)
-//        button.setTitleColor(.white, for: .normal)
-//        button.backgroundColor = .link
-//        button.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
-//        view.backgroundColor = .white
-//        view.addSubview(button)
+        let button = UIButton(frame: CGRect(x: 20, y: 200, width: view.frame.size.width - 40, height: 50))
+        button.setTitle("Add Entry", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .link
+        button.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
+        view.backgroundColor = .white
+        view.addSubview(button)
     }
     
     @objc private func addNewEntry() {
-        let object: [String: Any] = [
-            "name": "iOS Academy" as NSObject,
-            "Youtube": "yes"
-        ]
-        print("Clicked")
-        database.child("Something").setValue(object)
+        databaseManager.setInstance(of: "Bla Blah") {
+            print("hi")
+        }
     }
 
 
